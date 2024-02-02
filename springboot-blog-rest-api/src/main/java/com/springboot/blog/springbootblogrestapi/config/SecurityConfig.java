0312c8e687484f5d0c7e0fc19effa3c5e700c63f
Pinal2911 <pinalparmar270@gmail.com>
2024-02-02 23:59:47 +0530

    security

---
+++ springboot-blog-rest-api/src/main/java/com/springboot/blog/springbootblogrestapi/config/SecurityConfig.java
@@ -0,0 +1,64 @@
+package com.springboot.blog.springbootblogrestapi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+//this class is for basic authentication only
+//no form login will be there
+//only basic authentication
+//while testing via postman add password and username in authorization and do GET,POST,etc(any request)
+
+@Configuration
+//required below annotation to allow access to methods by roles
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig {
+
+    //encrypt the password using BCrypt
+    @Bean
+    public static PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http.csrf((csrf) -> csrf.disable())
+                //using below line will impose authentication to all requests
+                //.authorizeHttpRequests((authorize) -> authorize.anyRequest().authenticated())
+                //using below line will impose authentication to all requests except GET (put,POST,DELETE -> will require authentication)
+                .authorizeHttpRequests((authorize) ->
+                        authorize.requestMatchers(HttpMethod.GET,"/api/**")
+                                .permitAll()
+                                .anyRequest()
+                                .authenticated())
+                .httpBasic(Customizer.withDefaults());
+        return  http.build();
+    }
+    //defining various user roles for application
+    @Bean
+    public UserDetailsService userDetailsService(){
+        UserDetails pinal= User.builder()
+                .username("pinal")
+                .password(passwordEncoder().encode("pinal"))
+                .roles("USER")
+                .build();
+
+        UserDetails admin=User.builder()
+                .username("admin")
+                .password(passwordEncoder().encode("admin"))
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(pinal,admin);
+    }
+
+
+}
--- springboot-blog-rest-api/src/main/java/com/springboot/blog/springbootblogrestapi/controller/PostController.java
+++ springboot-blog-rest-api/src/main/java/com/springboot/blog/springbootblogrestapi/controller/PostController.java
@@ -8,6 +8,7 @@ import com.springboot.blog.springbootblogrestapi.utils.AppConstants;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -21,7 +22,10 @@ public class PostController {
         this.postService=postService;
     }
     //create a new post rest api
+    //pre authorizing the request and only admin has the access to this request
+
     @PostMapping
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<PostDto> createPost(@Valid  @RequestBody PostDto postDto){
         return new ResponseEntity<>(postService.createPost(postDto), HttpStatus.CREATED);
     }
@@ -48,6 +52,7 @@ public class PostController {
     }
 
     //udpate the post by id rest api
+    @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("/{id}")
     public ResponseEntity<PostDto> updatePost(@Valid @RequestBody PostDto postDto,@PathVariable(name="id") long id){
         PostDto postResponse=postService.updatePost(postDto,id);
@@ -55,6 +60,7 @@ public class PostController {
     }
 
     //delete the post by id rest api
+    @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("/{id}")
     public ResponseEntity<String> deletePost(@PathVariable(name="id")long id){
         postService.deleteById(id);
